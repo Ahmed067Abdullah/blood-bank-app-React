ae5e667d1c2711b15d9e807b3e4c612bcabc31ea
Ahmed Abdullah <ahmed067abdullah@gmail.com>
2019-02-17 15:05:43 +0500

    firebase credentials remoed

--- src/index.js
+++ src/index.js
@@ -12,12 +12,7 @@ import './index.css';
 
 // Initialize Firebase
 const config = {
-  apiKey: "AIzaSyBNYQLK9P1LK32jocz6Wds23Me6p0QrF_Y",
-  authDomain: "blood-bank-app-a0a41.firebaseapp.com",
-  databaseURL: "https://blood-bank-app-a0a41.firebaseio.com",
-  projectId: "blood-bank-app-a0a41",
-  storageBucket: "blood-bank-app-a0a41.appspot.com",
-  messagingSenderId: "49661168274"
+
 };
 initializeApp(config);
 
